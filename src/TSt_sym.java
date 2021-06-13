57a28fbd55ec1e150c0ff77d08ad5601ea4bb126
anitakamani <anitakamani@outlook.com>
2021-06-14 00:52:53 +0430

    keywords DONE!
    5 keywords added to the cup

--- src/TSt_sym.java
+++ src/TSt_sym.java
@@ -1,5 +1,7 @@
+
 public class TSt_sym {
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println(java.util.TimeZone.getDefault().getID());
+
 	}
 }
--- tst.java
+++ tst.java
@@ -1,5 +1,9 @@
 public class TSt_sym {
 	public static void main(String[] args) {
 		print;
+		clock;
+		location;
+		username;
+		os;
 	}
 }
